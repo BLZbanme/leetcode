6c056db23da311edb1787186e3ee450adca797a1
BLZbanme <swordcome@gmail.com>
2019-05-11 22:05:56 +0800

    '006'

---
+++ 006ZigZagConversion/demo.js
@@ -0,0 +1,10 @@
+var convert = function(s, numRows) {
+    var tmpArray = [];
+    for(let i = 0; i < numRows; i++){
+        tmpArray.push([]);
+    }
+    var charArray = s.splite('');
+    for(let i = 0; i < charArray.length; i++){
+        
+    }
+};
