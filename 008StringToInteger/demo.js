e7f22c8680309fe9af37d9612d92f4c23eb8aebd
BLZbanme <swordcome@gmail.com>
2019-05-14 10:32:29 +0800

    008

---
+++ 008StringToInteger/demo.js
@@ -0,0 +1,24 @@
+var myAtoi = function(str) {
+    debugger;
+    var num = parseInt(str);
+    if(!isNaN(num)){
+        if(num >= 2147483647){
+            return 2147483647;
+        }else if(num <= -2147483648){
+            return -2147483648;
+        }
+        return num;
+    }
+    return 0;
+};
+
+console.log(myAtoi("42"));
+console.log(myAtoi("    -42"));
+console.log(myAtoi("4193 with words"));
+console.log(myAtoi("words and 987"));
+console.log(myAtoi("-91283472332"));
+
+function trim(str){
+    return str.replace(/(^\s*)/g, "");
+}
+
