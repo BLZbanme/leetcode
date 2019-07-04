26ee7cfcd8c8ed79a9fb140a9c88e86800850936
BLZbanme <swordcome@gmail.com>
2019-07-04 15:48:41 +0800

    finish0073

--- 0001-0100/0051-0100/0073SetMatrixZeroes/demo.js
+++ 0001-0100/0051-0100/0073SetMatrixZeroes/demo.js
@@ -24,6 +24,66 @@ var setZeroes = function(matrix) {
     return matrix;
 };
 
+var setZeroes = function(matrix) {
+    let col0 = 1;
+    matrix.forEach((arr, i) => {
+        if(matrix[i][0] == 0){
+            col0 = 0;
+        }
+        tmp = [...arr];
+        tmp.shift();
+        tmp.forEach((v, j) => {
+            if(matrix[i][j + 1] == 0){
+                matrix[i][0] = matrix[0][j + 1] = 0;
+            }
+        })
+    })
+    for(let i = matrix.length - 1; i >= 0; i--){
+        for(let j = matrix[0].length - 1; j >= 1; j--){
+            if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                matrix[i][j] = 0;
+            }
+        }
+        if(col0 == 0) {
+            matrix[i][0] = 0;
+        }
+    }
+    return matrix;
+}
+
+var setZeroes = function(matrix) {
+    let col0 = 1, rows = matrix.length, cols = matrix[0].length;
+    for(let i = 0; i < rows; i++){
+        if(matrix[i][0] == 0){
+            col0 = 0;
+        }
+        for(let j = 1; j < cols; j++){
+            if(matrix[i][j] == 0){
+                matrix[i][0] = matrix[0][j] = 0;
+            }
+        }
+    }
+    for(let i = 1; i < rows; i++){
+        for(let j = 1; j < cols; j++){
+            if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    if(matrix[0][0] == 0) {
+        for(let i = 1; i < cols; i++){
+            matrix[0][i] = 0;
+        }
+    }
+    if(col0 == 0){
+        for(let i = 0; i < rows; i++){
+            matrix[i][0] = 0;
+        }
+    }
+    return matrix;
+}
+
+
 console.log(setZeroes([
     [1,1,1],
     [1,0,1],
