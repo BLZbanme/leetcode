1318abfd8371f321a8f8f475ede23ecf270db18b
BLZbanme <swordcome@gmail.com>
2019-07-23 12:19:22 +0800

    finish0095

---
+++ 0001-0100/0051-0100/0095UniqueBinarySearchTreesII/demo.js
@@ -0,0 +1,98 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number} n
+ * @return {TreeNode[]}
+ */
+var generateTrees = function(n) {
+    if (n === 0) {
+        return [];
+    }
+    let dp = new Array(n + 1);
+    for (let i = 0; i <= n; i++) {
+        dp[i] = new Array(n + 1);
+        dp[i][i] = [new TreeNode(i)];
+    }
+    for (let i = 2; i <= n; i++) {
+        for (let j = 1; j <= n - i + 1; j++) {
+            let tmp =  [];
+            multip([null], j, dp[j + 1][j + i - 1], tmp);
+            for (let k = j + 1; k < i + j - 1; k++) {
+                multip(dp[j][k - 1], k, dp[k + 1][j + i - 1], tmp); 
+            }
+            multip(dp[j][j + i - 2], j + i - 1, [null], tmp);
+            dp[j][j + i - 1] = tmp;
+        }
+    }
+    return dp[1][n];
+};
+
+function multip(leftTrees, nodeVal, rightTrees, result) {
+    // let newLeftTrees = JSON.parse(JSON.stringify(leftTrees));
+    // let newRightTrees = JSON.parse(JSON.stringify(rightTrees));
+    // for (let left of newLeftTrees) {
+    //     for (let right of newRightTrees) {
+    //         let newNode = new TreeNode(nodeVal);
+    //         newNode.left = left;
+    //         newNode.right = right;
+    //         result.push(newNode);
+    //     }
+    // }
+    for (let left of leftTrees) {
+        for (let right of rightTrees) {
+            let newNode = new TreeNode(nodeVal);
+            newNode.left = left;
+            newNode.right = right;
+            result.push(newNode);
+        }
+    }
+}
+
+var generateTrees = function(n) {
+    let result = [];
+    result[0] = [];
+    if (n == 0) {
+        return result[0];
+    }
+    result[0].push(null);
+    for (let len = 1; len <= n; len++) {
+        result[len] = [];
+        for (let j = 0; j < len; j++) {
+            for (let left of result[j]) {
+                for (let right of result[len - j - 1]) {
+                    let newNode = new TreeNode(j + 1);
+                    newNode.left = left;
+                    newNode.right = clone(right, j + 1);
+                    result[len].push(newNode);
+                }
+            }
+        }
+    }
+    return result[n];
+}
+
+function clone(node, offset) {
+    if (!node) {
+        return null;
+    }
+    let newNode = new TreeNode(node.val + offset);
+    newNode.left = clone(node.left, offset);
+    newNode.right = clone(node.right, offset);
+    return newNode;
+}
+
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
+console.log(generateTrees(0));
+console.log(generateTrees(2));
+console.log(generateTrees(3));
+console.log(generateTrees(4));
+console.log(generateTrees(5));
