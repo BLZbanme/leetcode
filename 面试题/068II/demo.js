ba85cfe2d35ba9fe544f41147b6b8119f37d606c
BLZbanme <swordcome@gmail.com>
2020-06-19 10:34:51 +0800

    finishlocf068II

--- 面试题/068II/demo.js
+++ 面试题/068II/demo.js
@@ -46,6 +46,114 @@ var lowestCommonAncestor = function(root, p, q) {
     return result;
 };
 
+var lowestCommonAncestor = function(root, p, q) {
+    let stack = [];
+    let cur = root;
+    let pre = null;
+
+    while (stack.length || cur) {
+        while (cur) {
+            stack.push(cur);
+            if (cur === p) {
+                break;
+            }
+            cur = cur.left
+        }
+
+        if (cur === p) {
+            break;
+        }
+
+        cur = stack[stack.length - 1];
+        if (cur.right && cur.right !== pre) {
+            cur = cur.right;
+        }
+        else {
+            pre = cur;
+            stack.pop();
+            cur = null;
+        }
+    }
+
+    let road1 = Array.from(stack);
+
+    stack = [];
+    cur = root;
+    pre = null;
+
+    while (stack.length || cur) {
+        while (cur) {
+            stack.push(cur);
+            if (cur === q) {
+                break;
+            }
+            cur = cur.left
+        }
+
+        if (cur === q) {
+            break;
+        }
+
+        cur = stack[stack.length - 1];
+        if (cur.right && cur.right !== pre) {
+            cur = cur.right;
+        }
+        else {
+            pre = cur;
+            stack.pop();
+            cur = null;
+        }
+    }
+
+    let road2 = Array.from(stack);
+    
+    let i = road1.length - 1;
+    while (i >= 0) {
+        if (road2.indexOf(road1[i]) !== -1) {
+            return road1[i];
+        }
+        i--;
+    }
+};
+
+var lowestCommonAncestor = function(root, p, q) {
+    const path2p = findPath(root, p);
+    const path2q = findPath(root, q);
+
+    let i = path2p.length - 1;
+    while (i >= 0) {
+        if (path2q.indexOf(path2p[i]) !== -1) {
+            return path2p[i];
+        }
+        i--;
+    }
+};
+
+function findPath(root, target) {
+    const stack = [];
+    let cur = root;
+    let pre = null;
+
+    while (stack.length || cur) {
+        while (cur) {
+            stack.push(cur);
+            if (cur === target) {
+                return stack;
+            }
+            cur = cur.left
+        }
+
+        cur = stack[stack.length - 1];
+        if (cur.right && cur.right !== pre) {
+            cur = cur.right;
+        }
+        else {
+            pre = cur;
+            stack.pop();
+            cur = null;
+        }
+    }
+}
 
 function TreeNode(val) {
     this.val = val;
