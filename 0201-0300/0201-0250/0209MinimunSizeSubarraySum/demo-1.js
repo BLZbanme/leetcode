0a81f8c01f97cc75b691f61842a31f065870a3de
BLZbanme <swordcome@gmail.com>
2020-06-28 11:27:15 +0800

    redo 0209

--- 0201-0300/0201-0250/0209MinimunSizeSubarraySum/demo-1.js
+++ 0201-0300/0201-0250/0209MinimunSizeSubarraySum/demo-1.js
@@ -1,81 +1,69 @@
-# 209. Minimum Size Subarray Sum
-
-Given an array of **n** positive integers and a positive integer **s**, find the minimal length of a **contiguous** subarray of which the sum ≥ **s**. If there isn't one, return 0 instead.
-
-**Example:** 
-
-```
-Input: s = 7, nums = [2,3,1,2,4,3]
-Output: 2
-Explanation: the subarray [4,3] has the minimal length under the problem constraint.
-```
-
-**Follow up:**
-
-If you have figured out the *O*(*n*) solution, try coding another solution of which the time complexity is *O*(*n* log *n*). 
-
-##### 2019.09.12
-
-##### 	我的思路：
-
-​		用一个数组记录，从某个点开始当前长度的序列的和，然后判断这个和是否大于等于s，若大于等于直接返回当前长度。
-
-```javascript
+/**
+ * @param {number} s
+ * @param {number[]} nums
+ * @return {number}
+ */
 var minSubArrayLen = function(s, nums) {
-    if (!nums) {
-        return 0;
+    const N = nums.length;
+    const dp = new Array(N);
+    for (let i = 0; i < N; i++) {
+        dp[i] = new Array(N);
+        dp[i][i] = nums[i];
+        if (nums[i] >= s) {
+            return 1;
+        }
     }
-    if (nums.some(e => e >= s)) {
-        return 1;
+
+    for (let i = 1; i < N; i++) {
+        for (let j = 0; i + j < N; j++) {
+            dp[j][j + i] = dp[j][j + i - 1] + nums[j + i];
+            if (dp[j][j + i] >= s) {
+                return i + 1;
+            }
+        }
     }
-    let dp = Array.from(nums);
-    for (let i = 1, N = nums.length; i < N; i++) {
-        for (let j = 0; j + i < N; j++) {
+
+    return 0;
+};
+
+var minSubArrayLen = function(s, nums) {
+    const N = nums.length;
+    const dp = new Array(N).fill(0);
+
+    for (let i = 0; i < N; i++) {
+        for (let j = 0; i + j < N; j++) {
             dp[j] += nums[j + i];
             if (dp[j] >= s) {
                 return i + 1;
-            } 
+            }
         }
     }
+
     return 0;
 };
-```
 
-##### 	别人的方法：
-
-##### 方法1：
-
-​	双指针法，用min记录当前的长度，从0点开始扩张数组的长度，发现数组的和大于等于s后，收缩数组的左边，得到最后大于s的值对应的长度就是当前子序列的长度！
-
-​	时间复杂度O(n)
-
-```javascript
 var minSubArrayLen = function(s, nums) {
     if (!nums || !nums.length) {
         return 0;
     }
+
     let i = 0;
     let j = 0;
     let sum = 0;
     let min = Infinity;
+
     while (j < nums.length) {
         sum += nums[j++];
+
         while (sum >= s) {
             min = Math.min(min, j - i);
             sum -= nums[i++];
         }
     }
+
     return min === Infinity ? 0 : min;
 }
-```
 
-##### 2020.06.28
-
-redo
-
-学到了新招数，二分查找（标准二分查找可以找到需要找大于等于该值的index！）
-
-```javascript
 var minSubArrayLen = function(s, nums) {
     const N = nums.length;
     const sums = new Array(N + 1);
@@ -119,5 +107,5 @@ function binarySearch(arr, target) {
 
     return lo;
 }
-```
 
+console.log(minSubArrayLen(7, [2,3,1,2,4,3]));
