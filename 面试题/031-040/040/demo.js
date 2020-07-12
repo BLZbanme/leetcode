c1396259d42ba09b466d5e830e4fbc02aec95cc3
BLZbanme <swordcome@gmail.com>
2020-07-12 23:48:44 +0800

    finish lcof 040

--- 面试题/031-040/040/demo.js
+++ 面试题/031-040/040/demo.js
@@ -40,6 +40,45 @@ function sink(arr, index, length) {
     }
 }
 
+var getLeastNumbers = function(arr, k) {
+    if (!k) {
+        return [];
+    }
+    let lo = 0;
+    let hi = arr.length - 1;
+
+    k--;
+
+    while (lo <= hi) {
+        let mid = partition(arr, lo, hi);
+        if (mid < k) {
+            lo = mid + 1;
+        }
+        else if (mid > k) {
+            hi = mid - 1;
+        }
+        else {
+            return arr.slice(0, k + 1);
+        }
+    }
+};
+
+function partition(arr, lo, hi) {
+    let i = lo;
+    let j = hi + 1;
+    while (true) {
+        while (arr[++i] < arr[lo] && i !== hi) {};
+        while (arr[--j] > arr[lo] && j !== lo) {};
+        if (i >= j) {
+            break;
+        }
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    [arr[lo], arr[j]] = [arr[j], arr[lo]];
+    return j;
+}
+
+
 console.log(getLeastNumbers([0,0,2,3,2,1,1,2,0,4], 10)); //[0,0,0,1,1,2,2,2,3,4]
 
 console.log(getLeastNumbers([3,2,1], 2)); //[1, 2]
