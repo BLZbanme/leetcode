a63162de986e33eacdb239a497c0c65e4544af85
BLZbanme <swordcome@gmail.com>
2020-11-19 00:37:01 +0800

    redo 0283

--- 0201-0300/0251-0300/0283MoveZeroes/demo-2.ts
+++ 0201-0300/0251-0300/0283MoveZeroes/demo-2.ts
@@ -1,25 +1,7 @@
-# 283. Move Zeroes
-
-Given an array `nums`, write a function to move all `0`'s to the end of it while maintaining the relative order of the non-zero elements.
-
-**Example:**
-
-```
-Input: [0,1,0,3,12]
-Output: [1,3,12,0,0]
-```
-
-**Note**:
-
-1. You must do this **in-place** without making a copy of the array.
-2. Minimize the total number of operations.
-
-#### 2020.11.19
-
-##### 	我的思路：
-
-```javascript
-function moveZeroes(nums: number[]): void {
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function moveZeroes1(nums: number[]): void {
     const N = nums.length;
     let i = 0;
     let j = 0;
@@ -35,11 +17,7 @@ function moveZeroes(nums: number[]): void {
         }
     }
 };
-```
 
-##### 别人的方法：
-
-```javascript
 function moveZeroes(nums: number[]): void {
     const N = nums.length;
     let i = 0;
@@ -53,5 +31,3 @@ function moveZeroes(nums: number[]): void {
     }
     return;
 }
-```
-
