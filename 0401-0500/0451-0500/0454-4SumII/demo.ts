79dd07f6f9a357b5a6743e103a371f295dd9153a
BLZbanme <swordcome@gmail.com>
2020-11-27 00:34:22 +0800

    finish 0454

---
+++ 0401-0500/0451-0500/0454-4SumII/demo.ts
@@ -0,0 +1,13 @@
+function fourSumCount(A: number[], B: number[], C: number[], D: number[]): number {
+    const countAB = new Map();
+    A.forEach(u => B.forEach(v => countAB.set(u + v, (countAB.get(u + v) || 0) + 1)));
+    let ans = 0;
+    for (let u of C) {
+        for (let v of D) {
+            if (countAB.has(- u - v)) {
+                ans += countAB.get(-u - v)
+            }
+        }
+    }
+    return ans;
+};
