c2a9b427934ea2fef900efd12ecd60f3fc967c91
BLZbanme <swordcome@gmail.com>
2020-12-04 00:33:45 +0800

    finish 0659

---
+++ 0601-0700/0651-0700/0659SplitArrayIntoConsecutiveSubsequences/demo.js
@@ -0,0 +1,38 @@
+function isPossible(nums) {
+    var countMap = new Map();
+    var endMap = new Map();
+    for (var _i = 0, nums_1 = nums; _i < nums_1.length; _i++) {
+        var x = nums_1[_i];
+        countMap.set(x, (countMap.get(x) || 0) + 1);
+    }
+    for (var _a = 0, nums_2 = nums; _a < nums_2.length; _a++) {
+        var x = nums_2[_a];
+        var count = countMap.get(x) || 0;
+        if (count > 0) {
+            var prevEndCount = endMap.get(x - 1) || 0;
+            if (prevEndCount > 0) {
+                countMap.set(x, count - 1);
+                endMap.set(x - 1, prevEndCount - 1);
+                endMap.set(x, (endMap.get(x) || 0) + 1);
+            }
+            else {
+                var count1 = countMap.get(x + 1) || 0;
+                var count2 = countMap.get(x + 2) || 0;
+                if (count1 > 0 && count2 > 0) {
+                    countMap.set(x, count - 1);
+                    countMap.set(x + 1, count1 - 1);
+                    countMap.set(x + 2, count2 - 1);
+                    endMap.set(x + 2, (endMap.get(x + 2) || 0) + 1);
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+;
+console.log(isPossible([1, 2, 3, 3, 4, 5])); //true
+console.log(isPossible([1, 2, 3, 3, 4, 4, 5, 5])); //true
+console.log(isPossible([1, 2, 3, 4, 4, 5])); //false
