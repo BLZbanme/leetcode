a9a9ea770440ae0fb230d26248d1dc3cce9edf90
BLZbanme <swordcome@gmail.com>
2020-12-14 00:25:37 +0800

    redo 0049

--- 0001-0100/0001-0050/0049GroupAnagrams/0049GroupAnagrams.md
+++ 0001-0100/0001-0050/0049GroupAnagrams/0049GroupAnagrams.md
@@ -76,3 +76,39 @@ var groupAnagrams = function(strs) {
 };
 ```
 
+#### 2020.12.14
+
+redo
+
+发现以前的测试用例有问题，没有考虑到各个字符超过10个的情况
+
+```typescript
+function groupAnagrams(strs: string[]): string[][] {
+    const map = new Map();
+    const aCode = 'a'.charCodeAt(0)
+    for (let str of strs) {
+        const arr = Array(26).fill(0)
+        for (let i = 0; i < str.length; i++) {
+            arr[str.charCodeAt(i) - aCode]++;
+        }
+        let strSet = ''
+        for (let i = 0; i < 26; i++) {
+            strSet += String.fromCharCode(i + aCode) + arr[i]
+        }
+        let tmp = map.get(strSet)
+        if (tmp) {
+            tmp.push(str)
+        }
+        else {
+            map.set(strSet, [str])
+        }
+    }
+    const keys = Array.from(map.keys());
+    const result = [];
+    for (let key of keys) {
+        result.push(map.get(key))
+    }
+    return result;
+};
+```
+
---
+++ 0001-0100/0001-0050/0049GroupAnagrams/demo-1.js
@@ -0,0 +1,33 @@
+"use strict";
+exports.__esModule = true;
+function groupAnagrams(strs) {
+    var map = new Map();
+    var aCode = 'a'.charCodeAt(0);
+    for (var _i = 0, strs_1 = strs; _i < strs_1.length; _i++) {
+        var str = strs_1[_i];
+        var arr = Array(26).fill(0);
+        for (var i = 0; i < str.length; i++) {
+            arr[str.charCodeAt(i) - aCode]++;
+        }
+        var strSet = '';
+        for (var i = 0; i < 26; i++) {
+            strSet += String.fromCharCode(i + aCode) + arr[i];
+        }
+        var tmp = map.get(strSet);
+        if (tmp) {
+            tmp.push(str);
+        }
+        else {
+            map.set(strSet, [str]);
+        }
+    }
+    var keys = Array.from(map.keys());
+    var result = [];
+    for (var _a = 0, keys_1 = keys; _a < keys_1.length; _a++) {
+        var key = keys_1[_a];
+        result.push(map.get(key));
+    }
+    return result;
+}
+;
+console.log(groupAnagrams(["bdddddddddd", "bbbbbbbbbbc"])); //[["bdddddddddd","bbbbbbbbbbc"]]
