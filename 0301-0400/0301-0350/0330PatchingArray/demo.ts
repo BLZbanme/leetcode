182b89b2022c9f5e2a63f1bacf9be55db82ff3b0
BLZbanme <swordcome@gmail.com>
2020-12-29 12:53:28 +0800

    do 0330

---
+++ 0301-0400/0301-0350/0330PatchingArray/demo.ts
@@ -0,0 +1,17 @@
+function minPatches(nums: number[], n: number): number {
+    let patches = 0;
+    let x = 1;
+    const length = nums.length;
+    let index = 0;
+    while (x <= n) {
+        if (index < length && nums[index] <= x) {
+            x += nums[index];
+            index++;
+        }
+        else {
+            x *= 2;
+            patches++;
+        }
+    }
+    return patches;
+};
