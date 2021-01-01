bbc6425b3840db938b3902687d9882998b1536c3
BLZbanme <swordcome@gmail.com>
2021-01-02 00:26:01 +0800

    finish 0239

---
+++ 0201-0300/0201-0250/0239SlidingWindowMaximum/demo.js
@@ -0,0 +1,30 @@
+function maxSlidingWindow(nums, k) {
+    var result = [];
+    var queue = [];
+    var N = nums.length;
+    for (var i = 0; i < k; i++) {
+        while (queue.length && queue[queue.length - 1] < nums[i]) {
+            queue.pop();
+        }
+        queue.push(nums[i]);
+    }
+    result.push(queue[0]);
+    for (var i = k; i < N; i++) {
+        if (nums[i - k] === queue[0]) {
+            queue.shift();
+        }
+        while (queue.length && queue[queue.length - 1] < nums[i]) {
+            queue.pop();
+        }
+        queue.push(nums[i]);
+        result.push(queue[0]);
+    }
+    return result;
+}
+;
+console.log(maxSlidingWindow([-7, -8, 7, 5, 7, 1, 6, 0], 4)); //[7,7,7,7,7]
+console.log(maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3)); //[3,3,5,5,6,7]
+console.log(maxSlidingWindow([1], 1)); //[1]
+console.log(maxSlidingWindow([1, -1], 1)); //[1, -1]
+console.log(maxSlidingWindow([9, 11], 2)); //[11]
+console.log(maxSlidingWindow([4, -2], 2)); //[4]
---
+++ 0201-0300/0201-0250/0239SlidingWindowMaximum/demo.ts
@@ -0,0 +1,30 @@
+function maxSlidingWindow(nums: number[], k: number): number[] {
+    const result: Array<number> = [];
+    const queue: Array<number> = [];
+    const N = nums.length;
+    for (let i = 0; i < k; i++) {
+        while (queue.length && queue[queue.length - 1] < nums[i]) {
+            queue.pop();
+        }
+        queue.push(nums[i]);
+    }
+    result.push(queue[0]);
+    for (let i = k; i < N; i++) {
+        if (nums[i - k] === queue[0]) {
+            queue.shift();
+        }
+        while (queue.length && queue[queue.length - 1] < nums[i]) {
+            queue.pop();
+        }
+        queue.push(nums[i]);
+        result.push(queue[0]);
+    }
+    return result;
+};
+
+console.log(maxSlidingWindow([-7,-8,7,5,7,1,6,0], 4)) //[7,7,7,7,7]
+console.log(maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3)) //[3,3,5,5,6,7]
+console.log(maxSlidingWindow([1], 1)) //[1]
+console.log(maxSlidingWindow([1, -1], 1)) //[1, -1]
+console.log(maxSlidingWindow([9, 11], 2)) //[11]
+console.log(maxSlidingWindow([4, -2], 2)) //[4]
