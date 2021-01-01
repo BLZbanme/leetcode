91512a3ebf539557f778e34172cb7b995c473433
BLZbanme <swordcome@gmail.com>
2021-01-01 23:36:38 +0800

    finish 0605

---
+++ 0601-0700/0601-0650/0605CanPlaceFlowers/demo.ts
@@ -0,0 +1,16 @@
+function canPlaceFlowers(flowerbed: number[], n: number): boolean {
+    if (!n) return true;
+    for (let i = 0; i < flowerbed.length; i++) {
+        if (flowerbed[i] === 0 && (i === 0 || flowerbed[i - 1] == 0) && (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)) {
+            flowerbed[i] = 1;
+            n--;
+            if (n <= 0) {
+                return true;
+            }
+        }
+    }
+    return n <= 0;
+};
+
+console.log(canPlaceFlowers([1, 0, 0, 0, 1], 1)) //true
+console.log(canPlaceFlowers([1, 0, 0, 0, 1], 2)) //false
