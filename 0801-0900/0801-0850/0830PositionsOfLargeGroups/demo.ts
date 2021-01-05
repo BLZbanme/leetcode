5e83e778dcc556ecfdf780d7cf17bafd0d67e66d
BLZbanme <swordcome@gmail.com>
2021-01-05 15:22:48 +0800

    finish 0830

---
+++ 0801-0900/0801-0850/0830PositionsOfLargeGroups/demo.ts
@@ -0,0 +1,21 @@
+function largeGroupPositions(s: string): number[][] {
+    const result = [];
+    const N = s.length;
+    let i = 0;
+    while (i < N) {
+        let j = 1;
+        while (i + j < N && s[i + j] === s[i]) {
+            j++;
+        }
+        if (j >= 3) {
+            result.push([i, i + j - 1])
+        }
+        i += j;
+    }
+    return result;
+};
+
+console.log(largeGroupPositions('abbxxxxzzy')) //[[3, 6]]
+console.log(largeGroupPositions('abc')) //[]
+console.log(largeGroupPositions('abcdddeeeeaabbbcd')) //[[3,5],[6,9],[12,14]]
+console.log(largeGroupPositions('aba')) //[]
