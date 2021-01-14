0d40e99bf865d19446c9f2697bd7adfbbeeaeb37
BLZbanme <swordcome@gmail.com>
2021-01-15 00:12:59 +0800

    fix

--- 1001-1100/1001-1050/1018BinaryPrefixDivisibleBy5/demo.js
+++ 1001-1100/1001-1050/1018BinaryPrefixDivisibleBy5/demo.js
@@ -1,14 +1,28 @@
-function prefixesDivBy5(A) {
+function prefixesDivBy51(A) {
     var N = A.length;
     var result = Array(N);
-    let pre = 0;
+    var pre = 0;
     for (var i = 0; i < N; i++) {
         pre <<= 1;
-        A[i] && (pre += 1)
-        pre %= 10;
+        pre = (pre + A[i]) % 10;
         result[i] = (pre === 0 || pre === 5);
     }
     return result;
 }
 ;
-console.log(prefixesDivBy5([0, 1, 1, 1, 1, 1])); //[true,false,false,false,true,false]
+function prefixesDivBy5(A) {
+    var N = A.length;
+    var result = Array(N);
+    var pre = 0;
+    for (var i = 0; i < N; i++) {
+        pre = ((pre << 1) + A[i]) % 5;
+        result[i] = !pre;
+    }
+    return result;
+}
+;
+console.log(prefixesDivBy5([0, 1, 1, 1, 1])); //[true,false,false,false,true,false]
+//0000
+//0101
+//1010
+//1111
