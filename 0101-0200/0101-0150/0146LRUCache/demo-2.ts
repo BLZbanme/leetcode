9a293589a400311a2282d8b621cf85935904cfa3
BLZbanme <swordcome@gmail.com>
2021-01-24 20:45:19 +0800

    restart 0146

---
+++ 0101-0200/0101-0150/0146LRUCache/demo-2.ts
@@ -0,0 +1,24 @@
+class LRUCache {
+    constructor(capacity: number) {
+        
+    }
+
+    get(key: number): number {
+
+    }
+
+    put(key: number, value: number): void {
+
+    }
+}
+
+class DListNode {
+    
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
