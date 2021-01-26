0f4ac899d2be833a0fd6920f5cfe92990a18dccf
BLZbanme <swordcome@gmail.com>
2021-01-26 22:28:54 +0800

    finish 0560

---
+++ 0501-0600/0551-0600/0560SubarraySumEqualsK/demo.js
@@ -0,0 +1,17 @@
+function subarraySum(nums, k) {
+    var map = new Map();
+    var n = nums.length;
+    var count = 0;
+    var tmp = 0;
+    map.set(0, 1);
+    for (var i = 0; i < n; i++) {
+        tmp += nums[i];
+        var diff = tmp - k;
+        count += map.get(diff) || 0;
+        map.set(tmp, (map.get(tmp) || 0) + 1);
+    }
+    return count;
+}
+;
+console.log(subarraySum([1, 1, 1], 2)); // 2
+console.log(subarraySum([1, 2, 3], 3)) // 2
