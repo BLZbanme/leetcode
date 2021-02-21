781ab9bb4bb78d04e86b072ad365e25e3dd85350
BLZbanme <swordcome@gmail.com>
2021-02-22 04:12:52 +0800

    finish 0766

---
+++ 0701-0800/0751-0800/0766ToeplitzMatrix/demo.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} matrix
+ * @return {boolean}
+ */
+var isToeplitzMatrix = function(matrix) {
+    const m = matrix.length, n = matrix[0].length;
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            if (matrix[i][j] != matrix[i - 1][j - 1]) {
+                return false;
+            }
+        }
+    }
+    return true;
+};
+
+console.log(isToeplitzMatrix([[18, 66]])) //true
+
+console.log(isToeplitzMatrix([[18],[66]])) //true
+
+
+console.log(isToeplitzMatrix([[1,2,3,4],[5,1,2,3],[9,5,1,2]])) //true
+console.log(isToeplitzMatrix([[1,2],[2,2]])) //false
