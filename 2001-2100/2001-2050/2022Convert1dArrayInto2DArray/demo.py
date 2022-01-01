3740e89749c27dfb4fa17ceb7c5d50091ed44069
BLZbanme <blzbanme@gmail.com>
2022-01-01 23:28:39 +0800

    finish 2022

---
+++ 2001-2100/2001-2050/2022Convert1dArrayInto2DArray/demo.py
@@ -0,0 +1,9 @@
+class Solution:
+    def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
+        N = len(original)
+        if N != m * n:
+            return []
+        dp = [[] * n for _ in range(m)]
+        for i in range(N):
+            dp[i // n][i % n] = original[i]
+        return dp
